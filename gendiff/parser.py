cbd24dc08b652d80f96fea54f3153a90dc8cd0bb
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-05-18 15:03:19 +0300

    Add -format argument

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -10,6 +10,7 @@ def create_parser():
         Namespace object with arguments.
     """
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
     return parser.parse_args()
