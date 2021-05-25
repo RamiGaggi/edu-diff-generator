3b96cf6f210f051eba41121ad2d259fae199fed4
Ramil Gadzhiev <rami.gaggi@gmail.com>
2021-05-25 11:56:48 +0300

    Add tests for plain format

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -4,17 +4,18 @@ import pytest
 from gendiff.gendiff import generate_diff
 
 
-def template(file1, file2, expect):
+def template(file1, file2, expect, style='stylish'):
     """Template for all tests.
 
     Args:
         file1 (str): Path to file1.
         file2 (str): Path to file2.
         expect (str): Expect result.
+        style (str): Output style
     """
     with open(expect) as file:
-        expect = file.read()
-    assert generate_diff(file1, file2) == expect
+        expect_file = file.read()
+    assert generate_diff(file1, file2, style) == expect_file
 
 
 def test_flat_json1():
@@ -63,6 +64,22 @@ def test_recursive():
     file2 = 'tests/fixtures/recursive/file2.yaml'
     expect = 'tests/fixtures/recursive/expect.txt'
     template(file1, file2, expect)
+
+
+def test_plain_format():
+    """Test 2 recursive json-yaml files for plain output."""
+    file1 = 'tests/fixtures/plain_format/pack1/file1.json'
+    file2 = 'tests/fixtures/plain_format/pack1/file2.yaml'
+    expect = 'tests/fixtures/plain_format/pack1/expect.txt'
+    template(file1, file2, expect, style='plain')
+
+
+def test_plain_format2():
+    """Test 2  json-yaml files for plain output."""
+    file1 = 'tests/fixtures/plain_format/pack2/file1.json'
+    file2 = 'tests/fixtures/plain_format/pack2/file2.yaml'
+    expect = 'tests/fixtures/plain_format/pack2/expect.txt'
+    template(file1, file2, expect, style='plain')
 
 
 def test_unsupported_format():
